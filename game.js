542da790c9773477f13103b62840109ab7b16c38
Dalton <dkrussell121@gmail.com>
2021-03-17 15:46:49 -0500

    Added game rules

---
+++ game.js
@@ -0,0 +1,75 @@
+//create function computerPlay return r,p,s
+//generate random 1 - 3
+//switch statement for return
+//returns rock, paper, or scissors randomly
+
+function computerPlay(){
+    let number = generateRandom();
+    switch(number){
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        default:
+            return "scissors";
+    }
+}
+
+function playerPlay(){
+    let noPlay = true;
+    while(noPlay){
+        let selection = prompt("Type Rock, Paper, or Scissors");
+        selection = selection.toLowerCase();
+        switch(selection){
+            case "rock":
+                return "rock";
+            case "paper":
+                return "paper";
+            case "scissors":
+                return "scissors";
+        }
+    }
+}
+
+function generateRandom(){
+    return Math.floor(Math.random()*3)+1;
+}
+
+const computerSelection = computerPlay();
+const playerSelection = playerPlay();
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "That was a tie."
+    }
+    if(playerSelection == "rock"){
+        if(computerSelection == "paper"){
+            return("You lose, " + computerSelection + " beats " + playerSelection + ".");
+        }
+        if(computerSelection == "scissors"){
+            return("You win, " + playerSelection + " beats " + computerSelection + ".");
+        }
+    }
+    if(playerSelection == "paper"){
+        if(computerSelection == "rock"){
+            return("You win, " + playerSelection + " beats " + computerSelection + ".");
+        }
+        if(computerSelection == "scissors"){
+            return("You lose, " + computerSelection + " beats " + playerSelection + ".");
+        }
+    }
+    if(playerSelection == "scissors"){
+        if(computerSelection == "rock"){
+            return("You lose, " + computerSelection + " beats " + playerSelection + ".");
+        }
+        if(computerSelection == "paper"){
+            return("You win, " + playerSelection + " beats " + computerSelection + ".");
+        }
+    }
+}
+
+console.log(playRound);
+
+
+
+
