5ab4dcdd3efe94b7e19a9a3f8526680290671369
Dalton <dkrussell121@gmail.com>
2021-03-17 16:24:15 -0500

    Game is playable

--- game.js
+++ game.js
@@ -1,8 +1,12 @@
-//create function computerPlay return r,p,s
-//generate random 1 - 3
-//switch statement for return
-//returns rock, paper, or scissors randomly
+//Rock, Paper, Scissors Game
+//Asks the user for their choice
+//Generates random choice for computer
+//Compares and prints round winner to console
+//Game lasts 5 rounds
+//Dalton Russell
+//3/17/2021
 
+//returns rock, paper, or scissors
 function computerPlay(){
     let number = generateRandom();
     switch(number){
@@ -15,6 +19,7 @@ function computerPlay(){
     }
 }
 
+//returns user input from prompt
 function playerPlay(){
     let noPlay = true;
     while(noPlay){
@@ -31,49 +36,80 @@ function playerPlay(){
     }
 }
 
+//returns 1, 2, or 3
 function generateRandom(){
     return Math.floor(Math.random()*3)+1;
 }
 
+//returns 0 for tie, 1 for player win, and 2 for computer win
 function playRound(playerSelection, computerSelection){
     if(playerSelection == computerSelection){
-        return "That was a tie."
+        return 0;
     }
     if(playerSelection == "rock"){
         if(computerSelection == "paper"){
-            return("You lose, " + computerSelection + " beats " + playerSelection + ".");
+            return 2;
         }
         if(computerSelection == "scissors"){
-            return("You win, " + playerSelection + " beats " + computerSelection + ".");
+            return 1;
         }
     }
     if(playerSelection == "paper"){
         if(computerSelection == "rock"){
-            return("You win, " + playerSelection + " beats " + computerSelection + ".");
+            return 1;
         }
         if(computerSelection == "scissors"){
-            return("You lose, " + computerSelection + " beats " + playerSelection + ".");
+            return 2;
         }
     }
     if(playerSelection == "scissors"){
         if(computerSelection == "rock"){
-            return("You lose, " + computerSelection + " beats " + playerSelection + ".");
+            return 2;
         }
         if(computerSelection == "paper"){
-            return("You win, " + playerSelection + " beats " + computerSelection + ".");
+            return 1;
         }
     }
 }
 
-
+//5 round game
+//prints score and outcome to console
 function game(){
+    let computerScore = 0;
+    let playerScore = 0;
     for(let i = 0; i < 5; i++ ){
         const computerSelection = computerPlay();
         const playerSelection = playerPlay();
-        console.log(playRound(playerSelection,computerSelection));
+        let  winner = playRound(playerSelection,computerSelection);
+        switch(winner){
+            case 0:
+                console.log("That was a tie.")
+                break;
+            case 1:
+                console.log("You win, " + playerSelection + " beats " + computerSelection + ".");
+                playerScore++;
+                break;
+            case 2:
+                console.log("You lose, " + computerSelection + " beats " + playerSelection + ".");
+                computerScore++;
+                break;
+        }
+    }
+    console.log("Game Over!!!");
+    console.log("Your Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+    if(playerScore > computerScore){
+        console.log("You are the winner.")
+    }
+    else if(computerScore > playerScore){
+        console.log("You are the loser.")
+    }
+    else {
+        console.log("Tie game.")
     }
 }
 
+//starts game
 game();
 
 
